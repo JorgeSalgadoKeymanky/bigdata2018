46fc8a47084fee532762d28a56f245de81e3200f
JorgeSalgadoKeymanky <42628215+JorgeSalgadoKeymanky@users.noreply.github.com>
2018-08-22 21:51:42 -0500

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#UPDATE SYSTEM
+
+	hostnamectl set-hostname Jorge
+	yum update -y
+
+#CREAR USER
+#INSTAL JAVA
+#INSTALL MVN
+#INSTALL PYTHON
+#INSTALL TMUX, HTOP
+#SINCRONIZAR LA HORA
